b9b45a1ed00dacf8f2f1d9b303c69059bd70fd31
Shreyas Patil <shreyaspatil367@gmail.com>
2025-12-02 22:10:15 +0530

    DOM Task-3

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-let h3 = document.querySelector('h3');
 let btn = document.querySelector('button');
-let inner = document.getElementById('inner');
-let grow = 0;
-
-
-function Time(num){
-    return (setInterval(()=>{   
-        grow++;
-        
-        h3.innerHTML = grow+'%';
-        inner.style.width = grow+'%';
-    },num));
-}
-
+let h3 = document.querySelector('h3')
+let flag = false;
+let card = document.getElementById('card');
+let icon = document.getElementById('love');
 
 btn.addEventListener('click',()=>{
-    let num = 50 + Math.floor(Math.random()*50);
-    console.log(`Your file will be dowloaded within ${num/10} second's`)
-    btn.style.pointerEvents = 'none';
+    if(flag){
+       btn.innerHTML = "Removing"
+        setTimeout(function(){
+            btn.innerHTML = "Follow"
+            btn.style.opacity = 1;
+            h3.innerHTML = 'Stranger'
+            h3.style.color = 'red'
+        },1000)
+        flag = false; 
+    }else{
+        btn.innerHTML = "Followed"
+        setTimeout(function(){
+            btn.innerHTML = "Remove Follower"
+            btn.style.opacity = 0.7;
+            h3.innerHTML = 'Friend'
+            h3.style.color = 'green'
+        },1000)
+        flag = true;
+    }
 
-    let inter = Time(num);
+})
 
-    setTimeout(()=>{
-        clearInterval(inter);
-        btn.style.opacity = 0.5;
-        btn.innerHTML = 'Dowloaded';
-    },num*100);
+card.addEventListener('dblclick',()=>{
+    // alert('hello');
+    icon.style.transform = 'translate(50%) scale(1) rotate(0deg)';
+    icon.style.opacity =1;
+
+    setTimeout(function(){
+        icon.style.transform = 'translate(0,-500%)';
+        icon.style.opacity = 0;
+    },500);
+
+    setTimeout(function(){
+        icon.style.transform = 'translate(50%) scale(0) rotate(-90deg)';
+    },1000);
 });
